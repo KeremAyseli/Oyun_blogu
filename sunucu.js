bcd967aba5d569e40dbac595c1ff750c1a7af6a0
KeremAyseli <46223333+KeremAyseli@users.noreply.github.com>
2020-05-02 05:06:24 +0300

    Dosyalar

---
+++ sunucu.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const router=express.Router();
+app.use(express.static('./'));
+app.get('/',function(req,res){
+     res.sendfile('./deneme.html')
+     res.append("deneme");
+    
+});
+app.get('/hakkinda.html',function(req,res){
+    res.sendfile('./hakkinda.html')
+    res.append("deneme");
+})
+
+app.listen(8888,function(){
+   console.log("8888 üstünden dinleniyor");
+})
